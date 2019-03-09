09fc371a371e89675fb25a6824ab4b058f23ffeb
AndyL <missedmemo@gmail.com>
2019-03-09 11:28:44 -0800

    simple func calls & looping

--- 2-cards/main.go
+++ 2-cards/main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	card := "Ace of Spades"
-	fmt.Println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Two of Diamonds")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
