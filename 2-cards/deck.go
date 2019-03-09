b307e0d6ab90ac0f871a938253257ebbac95fd2e
AndyL <missedmemo@gmail.com>
2019-03-09 14:12:37 -0800

    implement receiver and constructor equivalent

---
+++ 2-cards/deck.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// define deck as a slice of strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"diamonds", "spades", "hearts", "clubs"}
+	cardValues := []string{"one", "two", "three"}
+
+	for _, suit := range cardSuits {
+		for _, val := range cardValues {
+			cards = append(cards, val+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
--- 2-cards/main.go
+++ 2-cards/main.go
@@ -1,14 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Two of Diamonds")
+	cards := newDeck()
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
