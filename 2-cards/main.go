92a8ded6ba4f02dbd00da63bf8305cb27592b093
AndyL <missedmemo@gmail.com>
2019-03-09 20:21:37 -0800

    save to file

--- 2-cards/main.go
+++ 2-cards/main.go
@@ -2,11 +2,13 @@ package main
 
 func main() {
 	cards := newDeck()
+	cards.saveToFile("my_cards")
+	/*
+		hand, remainingCards := deal(cards, 5)
 
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
+		hand.print()
+		remainingCards.print()
+	*/
 }
 
 func newCard() string {
