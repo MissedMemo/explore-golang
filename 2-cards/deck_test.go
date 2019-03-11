e62913d42037c312d3c8ba250cce58f01fc395af
AndyL <missedmemo@gmail.com>
2019-03-11 13:17:12 -0700

    create additional unit tests

--- 2-cards/deck_test.go
+++ 2-cards/deck_test.go
@@ -11,4 +11,18 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != numCardsExpected {
 		t.Errorf("Expected %v cards in new deck, but got %v", numCardsExpected, len(d))
 	}
+
+	firstCard := d[0]
+	expectedFirstCard := "one of diamonds"
+
+	finalCard := d[len(d)-1]
+	expectedFinalCard := "three of clubs"
+
+	if firstCard != expectedFirstCard {
+		t.Errorf("Expected first card to be %v, but received %v", expectedFirstCard, firstCard)
+	}
+
+	if finalCard != expectedFinalCard {
+		t.Errorf("Expected final card to be %v, but received %v", expectedFinalCard, finalCard)
+	}
 }
