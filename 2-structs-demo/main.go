ff8e10db6fb319dbbffb49bb5d133586e96c1dd9
AndyL <missedmemo@gmail.com>
2019-03-11 21:44:18 -0700

    demo nested struct declaration & assignment

--- 2-structs-demo/main.go
+++ 2-structs-demo/main.go
@@ -2,16 +2,38 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	p1 := person{"John", "Smith"}
+	p1 := person{
+		"John",
+		"Smith",
+		contactInfo{
+			"john@gmail.com",
+			94604,
+		},
+	}
+
 	fmt.Println("p1", p1)
 
-	p2 := person{firstName: "Tom", lastName: "Jones"}
+	p2 := person{
+		firstName: "Tom",
+		lastName:  "Jones",
+		contact: contactInfo{
+			email:   "tom@yahoo.com",
+			zipCode: 12345,
+		},
+	}
+
 	fmt.Println("p2", p2)
 
 	var p3 person
