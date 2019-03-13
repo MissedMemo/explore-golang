206a15298cae4381b02c5aefe5f3460f000a4db1
AndyL <missedmemo@gmail.com>
2019-03-13 13:33:44 -0700

    implement get request using standard interfaces

---
+++ 4-interfaces/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	/*
+		bs := make([]byte, 99999)
+		resp.Body.Read(bs)
+		fmt.Println(string(bs))
+	*/
+
+	// above is same as...
+	io.Copy(os.Stdout, resp.Body)
+}
