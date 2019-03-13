3f1db1c5da9046a3b26f49925ff6dfe20b4fbab8
AndyL <missedmemo@gmail.com>
2019-03-13 11:00:07 -0700

    begin interfaces implementation

--- 4-interfaces/main.go
+++ 4-interfaces/main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
 func main() {
-	fmt.Println("Hello, interfaces!")
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func ( /* b */ englishBot) getGreeting() string {
+	return "Hello!"
+}
+
+func ( /* b */ spanishBot) getGreeting() string {
+	return "Hola!"
 }
