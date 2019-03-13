fa186e1e3193a680961a6187bb35b9f59600cccc
AndyL <missedmemo@gmail.com>
2019-03-12 20:10:27 -0700

    instantiate maps and assign values

--- 3-maps/main.go
+++ 3-maps/main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Let's try maps...")
+
+	colors1 := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+	}
+
+	fmt.Println(colors1)
+
+	// nil map (can't be assigned to)
+	//var colors2 map[string]string
+
+	colors3 := make(map[string]string)
+	colors3["gray1"] = "#7d7d7d"
+	colors3["gray2"] = "#b4b4b4"
+
+	fmt.Println(colors3)
+
+	delete(colors3, "gray2")
+	fmt.Println(colors3)
 }
