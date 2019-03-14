b8900d18271b19ad81cb61eafa3509c71a3de0a6
AndyL <missedmemo@gmail.com>
2019-03-14 13:22:43 -0700

    check status of common websites via synchronous GET requests

--- 5-channels/main.go
+++ 5-channels/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, channels!")
+
+	links := []string{
+		"http://google.com",
+		"http://amazon.com",
+		"http://golang.okg",
+		"http://yahoo.com",
+		"http://stackoverflow.com",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "may be down!")
+		return
+	}
+
+	fmt.Println(link, "is up...")
 }
