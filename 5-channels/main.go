7bf1b988f1dee55c95ada3660acea553309a9131
AndyL <missedmemo@gmail.com>
2019-03-14 17:51:08 -0700

    basic channel implementation

--- 5-channels/main.go
+++ 5-channels/main.go
@@ -15,17 +15,23 @@ func main() {
 		"http://stackoverflow.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "may be down!")
+		c <- "could be down..."
 		return
 	}
 
 	fmt.Println(link, "is up...")
+	c <- "seems fine!"
 }
